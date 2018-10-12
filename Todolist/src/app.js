1edc892e7a46955d49eecb5ded838d8c8f981116
BoboLee <bobo.li@mediawave.com.tw>
2018-10-12 18:21:49 +0800

    add click function(addTask, btnCancel)

--- Todolist/src/app.js
+++ Todolist/src/app.js
@@ -1,10 +1,30 @@
 import './sass/style.sass'
 // import './pug/index.pug'
+// new Vue({
+//   el: '#app',
+//   data: function () {
+//     return {
+//       visible: false
+//     }
+//   }
+// })
+
 new Vue({
   el: '#app',
-  data: function () {
-    return {
-      visible: false
+  data: {
+    tabPosition: 'bottom',
+    input: '',
+    value1: '',
+    textarea: ''
+  },
+  methods: {
+    openFrom: function () {
+      document.getElementById('addTask').classList.add('dis-no')
+      document.getElementById('addForm').classList.remove('dis-no')
+    },
+    btnCancel: function () {
+      document.getElementById('addForm').classList.add('dis-no')
+      document.getElementById('addTask').classList.remove('dis-no')
     }
   }
 })
