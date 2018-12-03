a0bde5e8a0f5b677861e35c6c4860e20ae212b41
admin <admin@example.com>
2018-12-03 18:15:49 +0800

    Modify checkout display process

--- Sweetaste/src/app.js
+++ Sweetaste/src/app.js
@@ -1,5 +1,18 @@
 import './sass/style.sass';
 new Vue({
     el: '#app',
-    data: {},
+    data: {
+        count: 0,
+        displayStatus: true,
+        btnStatus: true
+    },
+    methods: {
+        nextPage() {
+            this.count += 1
+            if (this.count === 2) {
+                this.btnStatus = false
+            }
+
+        }
+    }
 });
